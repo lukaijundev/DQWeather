f43d3d0960c27376dd176d33347dd8f3678fea38
lukaijun <616389781@qq.com>
2017-07-24 15:00:06 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/dqweather/db/Province.java
@@ -0,0 +1,45 @@
+package com.example.dqweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/7/24.
+ */
+
+public class Province extends DataSupport {
+
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+    private int getId(){
+
+        return id;
+    }
+
+
+}
